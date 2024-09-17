714e59724d716c0cdd826d20efcac0712e8d6b1e
alvichairofta <alviganteng14@gmail.com>
2024-09-17 18:58:24 +0700

    add baseline favorite icon

--- app/src/main/java/com/example/lab_week_04/DetailFragment.kt
+++ app/src/main/java/com/example/lab_week_04/DetailFragment.kt
@@ -5,19 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
+private const val COFFEE_ID = "COFFEE_ID"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [DetailFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class DetailFragment : Fragment() {
-    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
@@ -37,22 +32,56 @@ class DetailFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_detail, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Initialize views
+        val coffeeTitle: TextView = view.findViewById(R.id.coffee_title)
+        val coffeeDesc: TextView = view.findViewById(R.id.coffee_desc)
+
+        // Get coffee ID from arguments
+        val coffeeId = arguments?.getInt(COFFEE_ID, 0) ?: 0
+
+        // Set coffee data based on coffee ID
+        setCoffeeData(coffeeId, coffeeTitle, coffeeDesc)
+
+    }
+
+    private fun setCoffeeData(id: Int, coffeeTitle: TextView, coffeeDesc: TextView) {
+        when (id) {
+            R.id.affogato -> {
+                coffeeTitle.text = getString(R.string.affogato_title)
+                coffeeDesc.text = getString(R.string.affogato_desc)
+            }
+            R.id.americano -> {
+                coffeeTitle.text = getString(R.string.americano_title)
+                coffeeDesc.text = getString(R.string.americano_desc)
+            }
+            R.id.latte -> {
+                coffeeTitle.text = getString(R.string.latte_title)
+                coffeeDesc.text = getString(R.string.latte_desc)
+            }
+//            R.id.cappuccino -> {
+//                coffeeTitle.text = getString(R.string.cappuccino_title)
+//                coffeeDesc.text = getString(R.string.cappuccino_desc)
+//            }
+//            R.id.mocha -> {
+//                coffeeTitle.text = getString(R.string.mocha_title)
+//                coffeeDesc.text = getString(R.string.mocha_desc)
+//            }
+//            R.id.espresso -> {
+//                coffeeTitle.text = getString(R.string.espresso_title)
+//                coffeeDesc.text = getString(R.string.espresso_desc)
+//            }
+        }
+    }
+
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment DetailFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
+        fun newInstance(coffeeId: Int) =
             DetailFragment().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+                    putInt(COFFEE_ID, coffeeId)
                 }
             }
     }
