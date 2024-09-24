1c539460dfb9621ca723c150a7817a8781cfa755
alvichairofta <alviganteng14@gmail.com>
2024-09-25 01:41:13 +0700

    update part 3 and assignment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,5 +47,6 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
     implementation("androidx.navigation:navigation-fragment-ktx:2.8.0")
     implementation("androidx.navigation:navigation-ui-ktx:2.8.0")
+    implementation("androidx.viewpager2:viewpager2:1.0.0")
 
 }
